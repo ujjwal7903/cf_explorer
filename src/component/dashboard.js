3fde1abe7b5b6ecdec46f482d9610634c6675bf4
ujjwal7903 <ujjwal7903@gmail.com>
2021-06-11 19:24:10 +0530

    fixed-router

--- src/component/dashboard.js
+++ src/component/dashboard.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import Card from './Card';
 
+import {Redirect } from 'react-router-dom';
 
-const data = ["Card 1" , "Card 2" ,"Card 3", "Card 4" , "Card 5" ,"Card 6" ,"Card 7" , "Card 8" ,"Card 9","Card 10"];
+const data = ["Card 1", "Card 2", "Card 3", "Card 4", "Card 5", "Card 6", "Card 7", "Card 8", "Card 9", "Card 10"];
 
-const Dashboard = () =>{
-
-    return(
+function Dashboard ({authorized}) {
+    // if(!authorized) {
+    //     return <Redirect to="/landing" />
+    // }
+    return (
         <div className="dashboard">
-
-            <h1 style={{pading:'20px 20px', marginLeft:'600px'}}>CODE LIKE BEAST</h1>
+            <h1 style={{ pading: '20px 20px', marginLeft: '600px' }}>CODE LIKE BEAST</h1>
             <div className="user">umang_us</div>
-            
             <div className="container">
-            <div className="row">
-               {
-                   data.map(item =>{
-                       return(
-                            <div className="col-6">
-                                <Card show={item} />
-                            </div>
-                       );
-                   })
-               }
-               </div> 
+                <div className="row">
+                    {
+                        data.map(item => {
+                            return (
+                                <div className="col-6">
+                                    <Card show={item} />
+                                </div>
+                            );
+                        })
+                    }
+                </div>
             </div>
         </div>
     );
-    
+
 }
 
 export default Dashboard;
--- src/component/landing.js
+++ src/component/landing.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
-import {Button} from 'react-bootstrap';
+import { Route, Redirect } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import Dashboard from './dashboard';
 
+import { useHistory } from "react-router-dom";
 
+const Landing = () => {
 
-const Landing = () =>{
-    return(
-        <div className="landing-page" >
-           <div className="container landing-card">
-        
-                <input className="user-name" placeholder="user name" type="text" color="white" />
-                <Button variant="primary">Submit</Button>
-           </div>
-        </div>
+    let history = useHistory();
+    return (
+        <>
+            <div className="landing-page" >
+                <div className="container landing-card">
+
+                    <input className="user-name" placeholder="User name..." type="text" id="handleInp" color="white" />
+                    <button onclick={()=> {history.push("/dashboard");}} >
+                        Submit
+                    </button>
+                </div>
+            </div>
+        </>
     );
 }
 
